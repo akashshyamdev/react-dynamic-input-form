e771bb0a428d2a9402fa28277d43a8adf018a7bb
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-06 18:29:22 +0530

    🏷️ chore(input): added typing

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,13 +1,18 @@
-interface Input {
-    label: string;
-    value: string | number;
-    onChange: (value: string) => void;
-    validator?: (value: string) => boolean;
-    helperText?: string;
+export interface Input {
+  label: string;
+  value: string | number;
+  onChange: (value: string) => void;
+  validator?: (value: string | number) => boolean;
+  helperText?: string;
 }
 
 export interface SingleInputFormProps {
-    containerStyles?: {};
-    onDelete: (e: any) => void;
-    inputs: Input[];
+  containerStyles?: {};
+  onDelete: (e: any) => void;
+  inputs: Input[];
+}
+
+export interface InputProps {
+  name: string;
+  label: string;
 }
