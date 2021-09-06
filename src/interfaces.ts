b4e6e9424de20d46ff0d0b8ea933b11c18256b11
Akash Shyam <akash.shyam2008@gmail.com>
2021-09-06 17:16:44 +0530

    🏷️ chore(SingleInputForm): added typings

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,13 @@
-export interface CardProps {
-    text: string,
-    photo: string,
-    handleClick: Function
-};
+interface Input {
+    label: string;
+    value: string | number;
+    onChange: (value: string) => void;
+    validator?: (value: string) => boolean;
+    helperText?: string;
+}
+
+export interface SingleInputFormProps {
+    containerStyles?: {};
+    onDelete: (e: any) => void;
+    inputs: Input[];
+}
